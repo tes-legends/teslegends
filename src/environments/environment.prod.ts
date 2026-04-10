89d6e1b643e7bbdc5f32608f05dfed04e89e75fe
livingsacrifice <livingsacrifice121@gmail.com>
2026-04-10 08:52:21 -0500

    remove firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,7 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: "AIzaSyCGDt51PFL9QYQeoST0TOyZuQvP0u15dvw",
-  authDomain: "bacon2-bb9f2.firebaseapp.com",
-  projectId: "bacon2-bb9f2",
-  storageBucket: "bacon2-bb9f2.appspot.com",
-  messagingSenderId: "185344251803",
-  appId: "1:185344251803:web:87f2ffa1331576cd34333e"
-  }
+  production: true
 };
